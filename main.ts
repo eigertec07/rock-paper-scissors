cec901646f06a00bdca186d82248ee6f6f6ba247
eigertec07 <49597343+eigertec07@users.noreply.github.com>
2021-04-20 15:43:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-let hand = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    Opp2 = receivedNumber
+    basic.showNumber(Opp2)
+    if (hand > Opp2) {
+        if (hand > Opp2) {
+            music.playMelody("C5 - C5 C5 C5 - - - ", 159)
+        }
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     hand = randint(1, 3)
     if (hand == 1) {
@@ -28,7 +36,14 @@ input.onGesture(Gesture.Shake, function () {
             # # # # #
             `)
     }
+    if (hand == 1) {
+        hand = 4
+    }
+    radio.sendNumber(hand)
 })
+let hand = 0
+let Opp2 = 0
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
